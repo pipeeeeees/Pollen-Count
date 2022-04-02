2bfc1e206b274925f9a894f4d2a8ee8aa2e6f3c0
Pipes, David Henry Jr <pipeeeeees@gmail.com>
2022-04-02 15:23:39 -0400

    Initial commit

---
+++ pollen.py
@@ -0,0 +1,11 @@
+import webscraper as ws
+
+def getPollenCount():
+    mylist = ws.chunkParser(ws.scrape('https://www.atlantaallergy.com/pollen_counts'),
+                                      'class="pollen-num"').split(' ')
+    for i in mylist:
+        try:
+            j = float(i)
+            return j
+        except:
+            continue
